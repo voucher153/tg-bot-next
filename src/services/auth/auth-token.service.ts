0cc554b24283158110b48352f4141692a7056fea
IdelRamazanov <vouchercomm227@gmail.com>
2024-08-15 13:41:15 +0300

    fix

--- src/services/auth/auth-token.service.ts
+++ src/services/auth/auth-token.service.ts
@@ -10,7 +10,7 @@ export const getAccessToken = () => {
 export const saveTokenStorage = (accessToken: string) => {
     Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
         domain: process.env.DOMAIN,
-        sameSite: 'strict',
+        sameSite: 'Lax',
         expires: 1
     })
 }
