cc2e7e5e94b26a28f506d4464d3c0d397fcc1073
IdelRamazanov <vouchercomm227@gmail.com>
2024-08-15 22:06:44 +0300

    fix

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function AuthLayout({children}: {children: React.ReactNode}) {
 
     return (
-        <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+        <div className="relative flex h-screen w-screen flex-col md:items-center md:justify-center md:bg-transparent">
             {children}
         </div>
     )
