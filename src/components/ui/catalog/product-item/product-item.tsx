c71da92cc776eddbac8ae52b288db4342f95f47f
IdelRamazanov <vouchercomm227@gmail.com>
2024-08-26 01:48:53 +0300

    fix

--- src/components/ui/catalog/product-item/product-item.tsx
+++ src/components/ui/catalog/product-item/product-item.tsx
@@ -29,7 +29,7 @@ export const ProductItem: FC<{product: IProduct}> = ({product}) => {
                 <Image 
                     src={product.imageUrl} 
                     width={130} 
-                    height={120} 
+                    height={130} 
                     alt={product.name} 
                     className={s.img}
                 />
