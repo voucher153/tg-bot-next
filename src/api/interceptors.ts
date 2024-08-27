aad23cb74bf97f3686464277e186ae64a9cc9593
IdelRamazanov <vouchercomm227@gmail.com>
2024-08-27 16:04:13 +0300

    fix

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -7,7 +7,7 @@ import { getAccessToken, removeTokenFromStorage } from '../services/auth/auth-to
 
 const options: CreateAxiosDefaults = {
     //baseURL: 'https://tg-bot-next-back.onrender.com/',
-    baseURL: process.env.SERVER_URL,
+    baseURL: process.env.SERVER_URL || "https://tg-bot-next-back-1.onrender.com/",
     headers: getContentType(),
     withCredentials: true
 }
